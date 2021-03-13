081e4130f162e8bb89097ce607238bed61a8ef2b
laminedabo <laminedabo88@gmail.com>
2021-03-13 17:59:02 +0000

    disabling capacitor

--- projet_mobile/src/app/here-map/here-map.page.ts
+++ projet_mobile/src/app/here-map/here-map.page.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActionSheetController, Platform, AlertController } from '@ionic/angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsMapTypeId,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
+
 
 @Component({
   selector: 'app-here-map',
@@ -8,9 +21,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HereMapPage implements OnInit {
 
-  constructor() { 
+  map: GoogleMap;
+
+  constructor(
+    public alertController: AlertController,
+    public actionCtrl: ActionSheetController,
+    private platform: Platform
+  ) { 
+    if (this.platform.is('cordova')) {
+      this.loadMap();
+    }
   }
 
   ngOnInit() {
+  }
+
+  loadMap() {
+    Environment.setEnv({
+      API_KEY_FOR_BROWSER_RELEASE: 'AIzaSyCUNjWbM-pWdpwELzdjsj7R1FhgwVGRmtw',
+      API_KEY_FOR_BROWSER_DEBUG: 'AIzaSyCUNjWbM-pWdpwELzdjsj7R1FhgwVGRmtw'
+    });
+    this.map = GoogleMaps.create('map_canvas', {
+      camera: {
+        target: {
+          lat: 14.758781,
+          lng: -17.437819
+        },
+        zoom: 12,
+        tilt: 30
+      }
+    });
   }
 }
